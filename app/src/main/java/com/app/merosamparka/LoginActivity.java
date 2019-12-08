d4b7323c595b376ca4094bf0b5bba2da44897b64
angela <angelnewa101@gmail.com>
2019-12-08 13:51:34 +0545

    signup form design

--- app/src/main/java/com/app/merosamparka/LoginActivity.java
+++ app/src/main/java/com/app/merosamparka/LoginActivity.java
@@ -14,7 +14,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText Password;
     private TextView Info;
     private Button Login;
-    private int counter = 5;
+    private int counter = 8;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
