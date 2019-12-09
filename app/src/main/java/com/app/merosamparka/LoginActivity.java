7714e5d8a69aeb4259a353aa322afebd4901e717
angela <angelnewa101@gmail.com>
2019-12-09 14:22:20 +0545

    signup form design

--- app/src/main/java/com/app/merosamparka/LoginActivity.java
+++ app/src/main/java/com/app/merosamparka/LoginActivity.java
@@ -4,53 +4,86 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
-    private EditText Name;
-    private EditText Password;
-    private TextView Info;
-    private Button Login;
-    private int counter = 8;
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    EditText etName, etPassword;
+    Button btnLogin, btnSignup, btnForgot;
+
+    RelativeLayout relay1, relay2;
+    Handler handler = new Handler();
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            relay1.setVisibility(View.VISIBLE);
+            relay2.setVisibility(View.VISIBLE);
+        }
+    };
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Name = findViewById(R.id.etName);
-        Password = findViewById(R.id.etPassword);
-        Info = findViewById(R.id.tvInfo);
-        Login = findViewById(R.id.btnLogin);
 
-        Info.setText("No of attempts remaining: 5");
+        etName = findViewById(R.id.etName);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnSignup = findViewById(R.id.btnSignup);
+        btnForgot = findViewById(R.id.btnForgot);
 
-        Login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                validate(Name.getText().toString(), Password.getText().toString());
 
-            }
-        });
+
+        btnLogin.setOnClickListener(this);
+
+
+        relay1 = findViewById(R.id.relay1);
+        relay2 = findViewById(R.id.relay2);
+
+//Setting timeout for the splash
+        handler.postDelayed(runnable, 2000);
+
+
     }
 
-    private void validate(String userName, String userPassword) {
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnLogin:
+                if (etName.getText().toString().equals("admin")&& etPassword.getText().toString().equals("admin")){
+                    startActivity(new Intent(this, Dashboard.class));
+                }
+                else if (TextUtils.isEmpty(etName.getText())){
+                    etName.setError("Please enter your Name");
+                    return;
+                }
 
-        if ((userName.equals("admin")) && (userPassword.equals("admin"))) {
-            Intent intent = new Intent(LoginActivity.this, Dashboard.class);
-            startActivity(intent);
-        } else {
-            counter--;
+                else if (TextUtils.isEmpty(etPassword.getText())){
+                    etPassword.setError("Please enter your Password");
+                    return;
+                }
+                else{
+                    Toast.makeText(this, "Invalid Name or Password", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                break;
 
-            Info.setText("No of attempts remaining: " + String.valueOf(counter));
-
-            if (counter == 0) {
-                Login.setEnabled(false);
-            }
-
+            case R.id.btnSignup:
+                startActivity(new Intent(this, SignupActivity.class));
+                break;
         }
+
+
     }
 }
